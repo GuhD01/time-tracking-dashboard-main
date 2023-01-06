f4569a5961b3bfd9048ba3a7608d7e69c508500a
GuhD <88705355+GuhD01@users.noreply.github.com>
2023-01-06 16:02:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const daily = document.querySelector("#daily");
+const weekly = document.querySelector("#weekly");
+const monthly = document.querySelector("#monthly");
+
+function handleClick(event) {
+  const selectedPeriod = event.target.id;
+  const periods = ['daily', 'weekly', 'monthly'];
+
+  periods.forEach((period) => {
+    const elements = document.querySelectorAll(`.${period}`);
+    if (period === selectedPeriod) {
+      elements.forEach((element) => element.classList.remove("hide"));
+      event.target.classList.add("selected");
+    } else {
+      elements.forEach((element) => element.classList.add("hide"));
+      document.querySelector(`#${period}`).classList.remove("selected");
+    }
+  });
+}
+
+daily.addEventListener("click", handleClick);
+weekly.addEventListener("click", handleClick);
+monthly.addEventListener("click", handleClick);
 
